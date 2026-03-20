49676c93bca4ec34e82ac7d71edda219a8e9f9e2
Bharathz <bharath8101999@gmail.com>
2026-03-20 09:41:55 +0530

    Class 15

---
+++ JavaScript/Class 15/index.js
@@ -0,0 +1,61 @@
+// Syncronous code => this code executes line by line
+
+// console.log("before");
+
+
+
+// function fn(){
+// // for(let i=0; i<1000000000; i++){
+// //     // some heavy task
+// //     console.log("From Loop : ",i);
+// // }
+//     console.log("inside function");
+// }
+// fn();
+// console.log("after");
+
+
+// Asyncronous code => this code does not execute line by line, it executes when the event loop is empty
+
+// console.log("before");
+// function fn(){
+// for(let i=0; i<100000; i++){
+//     // some heavy task
+//     if(i === 99999){
+//         console.log("From Loop : ",i);
+//     }
+// }
+//     console.log("inside function");
+// }
+
+// setTimeout(fn, 1000); 
+// // setTimeout is an asynchronous function that executes the callback function 
+// // after the specified time (in milliseconds) has elapsed.
+// console.log("after");
+
+
+//call back function => a function that is passed as an argument to another function and is executed after some operation is completed.
+
+// let a=true;
+// console.log("before");
+// setTimeout(()=>{
+//     a=false;
+//     console.log("inside setTimeout I will break the while");
+// },1000);
+// console.log("after");
+
+// while(a){
+//     a=false;
+// }
+
+
+console.log("Before");
+const cb2 = () => {
+    console.log("set timeout 1");
+    // while(1){}
+};
+
+const cb1 = () => console.log("Hello");
+setTimeout(cb2, 1000);
+setTimeout(cb1, 2000);
+console.log("After");
