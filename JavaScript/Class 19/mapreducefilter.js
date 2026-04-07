5ef52c99a2a45aa96a2d3c84f41cbbfda188c41a
Bharathz <bharath8101999@gmail.com>
2026-04-07 07:31:50 +0530

    updated

--- JavaScript/Class 19/mapreducefilter.js
+++ JavaScript/Class 19/mapreducefilter.js
@@ -1,4 +1,8 @@
-// map | filter | reduce
+// map | filter | reduce 
+//these are higher order functions that are used to manipulate arrays in JavaScript.
+// these dont mutate the original array but return a new array or a single value based 
+// on the operation performed.
+
 const arr = [1, 2, 3, 4, 5];
 
 // map
